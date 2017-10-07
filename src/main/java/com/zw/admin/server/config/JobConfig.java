b568b73701893b1092a8109110280409b70e1e37
zwzw1219 <zwzw1219@192.168.1.111>
2017-10-07 11:18:57 +0800

    1

--- src/main/java/com/zw/admin/server/config/JobConfig.java
+++ src/main/java/com/zw/admin/server/config/JobConfig.java
@@ -2,19 +2,13 @@ package com.zw.admin.server.config;
 
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-
-import com.zw.admin.server.model.JobModel;
-import com.zw.admin.server.service.JobService;
 
 @Configuration
 public class JobConfig {
@@ -39,28 +33,28 @@ public class JobConfig {
 		return quartzScheduler;
 	}
 
-	@Autowired
-	private JobService jobService;
-	@Autowired
-	private TaskExecutor taskExecutor;
-
-	/**
-	 * 初始化一个定时删除日志的任务
-	 */
-	@PostConstruct
-	public void initDeleteLogsJob() {
-		taskExecutor.execute(() -> {
-			JobModel jobModel = new JobModel();
-			jobModel.setJobName("delete-logs-job");
-			jobModel.setCron("0 0 0 * * ?");
-			jobModel.setDescription("定时删除三个月前日志");
-			jobModel.setSpringBeanName("sysLogServiceImpl");
-			jobModel.setMethodName("deleteLogs");
-			jobModel.setIsSysJob(true);
-			jobModel.setStatus(1);
-
-			jobService.saveJob(jobModel);
-		});
-	}
+//	@Autowired
+//	private JobService jobService;
+//	@Autowired
+//	private TaskExecutor taskExecutor;
+//
+//	/**
+//	 * 初始化一个定时删除日志的任务
+//	 */
+//	@PostConstruct
+//	public void initDeleteLogsJob() {
+//		taskExecutor.execute(() -> {
+//			JobModel jobModel = new JobModel();
+//			jobModel.setJobName("delete-logs-job");
+//			jobModel.setCron("0 0 0 * * ?");
+//			jobModel.setDescription("定时删除三个月前日志");
+//			jobModel.setSpringBeanName("sysLogServiceImpl");
+//			jobModel.setMethodName("deleteLogs");
+//			jobModel.setIsSysJob(true);
+//			jobModel.setStatus(1);
+//
+//			jobService.saveJob(jobModel);
+//		});
+//	}
 
 }

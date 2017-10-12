ddab43170142cbf9f728f2486f032edaec1b76ff
wei.zhang2 <wei.zhang2@baidao.com>
2017-10-12 16:02:27 +0800

    1

--- src/test/java/com/zw/admin/server/ServerApplicationTest.java
+++ src/test/java/com/zw/admin/server/ServerApplicationTest.java
@@ -1,5 +1,6 @@
 package com.zw.admin.server;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,4 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ServerApplicationTest {
 
+	@Test
+	public void test1() {
+
+	}
 }
